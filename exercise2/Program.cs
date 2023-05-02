dd6c6e0ab82105cf9dda19cc996904ac5751d30e
Robert <ishbuldinrobert2002@gmail.com>
2023-05-02 13:11:28 +0500

    exercise2 complate

---
+++ exercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} > {b}");
+}
+else if (a < b) // без else при выполнении первого условия выполнялось третье
+{
+    Console.WriteLine($"{a} < {b}");
+}
+else
+{
+    Console.WriteLine($"Числа {a} и {b} равны)");
+}
